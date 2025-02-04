fcf87989aeecdc778b60dee844b3c858024b6779
Thales Eller <thales.r.eller@gmail.com>
2025-02-03 18:28:04 -0800

    Add last question and answer tracking to session state

--- app/schemas.py
+++
@@ -1,7 +0,0 @@
-from pydantic import BaseModel, Field
-
-class Play(BaseModel):
-    play: str = Field(
-        description='Next play from the player.')
-    board: dict = Field(
-        description='Current board.')
