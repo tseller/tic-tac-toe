368b1d797cc1d9c5f7795b4ea6bb6758b2bea9a0
Thales Eller <thales.r.eller@gmail.com>
2025-02-16 19:03:31 -0800

    storing whatever

--- app/gemini.py
+++ app/gemini.py
@@ -1,9 +1,16 @@
 import vertexai
 from vertexai.generative_models import GenerativeModel
+from vertexai.preview.vision_models import ImageGenerationModel
 from floggit import flog
 
 PROJECT_ID = "learned-grammar-426001-g1"
 vertexai.init(project=PROJECT_ID, location="us-west1")
+
+image_model = ImageGenerationModel.from_pretrained("imagen-3.0-generate-001")
+@flog
+def generate_image(prompt):
+    response = image_model.generate_images(prompt)
+    return response.images[0].show()
 
 model = GenerativeModel("gemini-1.5-flash-002")
 @flog
